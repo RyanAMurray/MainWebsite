90e6e471d03e2de9190e63e06a31ec3d38aac93f
RyanAMurray <82988738+RyanAMurray@users.noreply.github.com>
2023-06-01 11:47:40 -0500

    Delete script.js

--- script.js
+++
@@ -1,4 +0,0 @@
-window.addEventListener('scroll', function() {
-    var scrollingImage = document.querySelector('.scrolling-image');
-    scrollingImage.style.transform = 'translateY(' + window.pageYOffset + 'px)';
-});
